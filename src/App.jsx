cb23d808f271c0d3e48dc9cfad10533254f0a93d
imarialu <marialuizaprado13@gmail.com>
2026-05-02 13:56:11 -0300

    feat: adiciona rotas publicas e privadas

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,26 @@ import MyBooks from './pages/MyBooks/MyBooks';
 import MyAccount from './pages/MyAccount/MyAccount';
 import BookDetails from './pages/BookDetails/BookDetails';
 import EditBook from './pages/EditBook/EditBook';
+import PrivateRoute from './layouts/PrivateRoute';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          {/* Rotas públicas */}
           <Route path='/login' element={<Login/>}/> 
           <Route path='/cadastro' element={<Register/>}/> 
-          
-          <Route index element={<Home/>}/> 
-          <Route path='/cadastro-de-livros' element={<BookRegister/>}/> 
-          <Route path='/meus-livros' element={<MyBooks/>}/> 
-          <Route path='/minha-conta' element={<MyAccount/>}/> 
-          <Route path='/detalhes/:uuid' element={<BookDetails/>}/> 
-          <Route path='/editar-livro/:uuid' element={<EditBook/>}/> 
+
+          {/* Rotas privadas */}
+          <Route path='/' element={<PrivateRoute/>}>
+            <Route index element={<Home/>}/> 
+            <Route path='/cadastro-de-livros' element={<BookRegister/>}/> 
+            <Route path='/meus-livros' element={<MyBooks/>}/> 
+            <Route path='/minha-conta' element={<MyAccount/>}/> 
+            <Route path='/detalhes/:uuid' element={<BookDetails/>}/> 
+            <Route path='/editar-livro/:uuid' element={<EditBook/>}/> 
+          </Route>
         </Routes>
       </Router>
     </>
