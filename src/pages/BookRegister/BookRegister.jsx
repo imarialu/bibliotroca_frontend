ad220b3b3fce01c2ee64f1d7907660c5d81fad9f
imarialu <marialuizaprado13@gmail.com>
2026-05-02 16:54:35 -0300

    feat: adiciona função de registrar e editar livro

--- src/pages/BookRegister/BookRegister.jsx
+++ src/pages/BookRegister/BookRegister.jsx
@@ -1,12 +1,73 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+import url from "../../services/url";
+import api from "../../services/api";
+import { getGenre } from "../../services/bookService";
+import { getUser } from "../../services/userService";
+
 import MainLayout from "../../layouts/MainLayout";
 import Heading from "../../components/Heading";
 import { Input } from "../../components/Input";
 import FilledButton from "../../components/FilledButton";
 
 export default function BookRegister(){
+    const [genres, setGenres] = useState([]);
+    const [imagem, setImagem] = useState(null);
+    const [formData, setFormData] = useState({
+        titulo: "",
+        autor: "",
+        paginas: "",
+        categoria_uuid: "",
+        estado: "",
+        trocado: false,
+        usuario: "",
+        descricao: "",
+        ativo: true
+    });
+    console.log(formData);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchBook = async () => {
+            const response = await getGenre();
+            setGenres(response.data);
+        }
+
+        fetchBook();
+    }, []);
+
+    const handleRegisterBook = async (e) => {
+        e.preventDefault();
+        try{
+            const fd = new FormData();
+
+            fd.append("livro", JSON.stringify(formData));
+
+            fd.append("imagem", imagem);
+            
+            const response = await api.post('/livros', 
+                fd, 
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`,
+                    },
+                }
+            );
+            
+            navigate('/meus-livros');
+        }catch(error){
+            console.log("Erro ao cadastrar livro: ", error);
+        }
+    };
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
     return(
         <>
             <MainLayout>
@@ -15,7 +76,7 @@ export default function BookRegister(){
                 </header>
 
                 <section className="md:ml-20 w-[80%] md:w-[720px] xl:w-[1100px]">
-                    <form onSubmit={} className="flex flex-col p-8 mb-10 bg-white rounded-sm shadow-cont sm:mb-0">
+                    <form onSubmit={handleRegisterBook} className="flex flex-col p-8 mb-10 bg-white rounded-sm shadow-cont sm:mb-0">
                         <div className="flex flex-col md:flex-row xl:grid grid-cols-3">
                             <div className="flex flex-col justify-between mb-10 md:mb-0 md:mr-8 xl:col-span-1 items-center">
                                 <div className="w-[220px] h-[280px] mb-5 md:mb-0 md:w-[230px] md:h-[330px] rounded-sm bg-gray">
@@ -23,7 +84,13 @@ export default function BookRegister(){
                                </div>
 
                                 <div className="flex w-[230px] md:w-[230px] rounded-full justify-center items-center py-1 px-10 bg-purple-tr text-purple font-semibold cursor-pointer transition duration-400 ease hover:bg-purple-h">
-                                    <input type="file" id="imagem" name="imagem" className="hidden" onChange={}/>
+                                    <input 
+                                        type="file" 
+                                        id="imagem" 
+                                        name="imagem" 
+                                        className="hidden" 
+                                        onChange={(e) => setImagem(e.target.files[0])}
+                                    />
                                     <label htmlFor="imagem" className="">Selecionar imagem</label>
                                 </div>
                             </div>
@@ -33,22 +100,30 @@ export default function BookRegister(){
                                     <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4">
                                         <Input 
                                             required
-                                            label={"Título"}
+                                            label={"Título"} 
+                                            name="titulo" 
+                                            onChange={handleChange}
                                         />
                                         <Input 
                                             required
-                                            label={"Autor"}
+                                            label={"Autor"} 
+                                            name="autor" 
+                                            onChange={handleChange}
                                         />
                                     </div>
                                     <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4">
                                         <Input 
                                             required
-                                            label={"Nº de páginas"}
+                                            label={"Nº de páginas"} 
+                                            name="paginas" 
+                                            onChange={handleChange}
                                         />
                                         <div className="flex flex-col">
                                             <label htmlFor="genre" className="text-lg">Gênero</label>
                                             <select 
-                                                required 
+                                                required
+                                                name="categoria_uuid" 
+                                                id="genre" 
                                                 onChange={handleChange}
                                                 className="w-[325px] h-8 mt-1 mb-3 pl-2 border border-gray rounded-sm shadow-xs focus:outline-none focus:border-purple md:w-[190px] xl:w-[340px]">
                                                     <option value="">Selecione o gênero</option>
@@ -67,24 +142,33 @@ export default function BookRegister(){
                                     <div className="flex gap-4 my-2">
                                         <label htmlFor="newBook" className="flex gap-2">
                                             <input 
-                                                type="radio"
-                                                onChange={}
+                                                type="radio" 
+                                                name="estado" 
+                                                id="newBook" 
+                                                value="novo"
+                                                onChange={handleChange}
                                             />
                                                 Novo
                                         </label>
                                         
                                         <label htmlFor="likeNew" className="flex gap-2">
                                             <input 
-                                                type="radio"
-                                                onChange={}
+                                                type="radio" 
+                                                name="estado" 
+                                                id="likeNew" 
+                                                value="seminovo"
+                                                onChange={handleChange}
                                             />
                                                 Seminovo
                                         </label>
 
                                         <label htmlFor="oldBook" className="flex gap-2">
                                             <input 
-                                                type="radio"
-                                                onChange={}
+                                                type="radio" 
+                                                name="estado" 
+                                                id="oldBook" 
+                                                value="antigo" 
+                                                onChange={handleChange}
                                             />
                                             Antigo
                                         </label>
@@ -94,9 +178,10 @@ export default function BookRegister(){
                                         <label htmlFor="description" className="text-lg mb-1">Descrição sobre o livro</label>
                                         <textarea 
                                             name="descricao" 
+                                            id="description" 
                                             rows="5" 
                                             className="resize-none p-2 border border-gray rounded-sm shadow-xs focus:outline-none focus:border-purple" 
-                                            onChange={}>
+                                            onChange={handleChange}>
                                         </textarea>
                                     </div>
                                 </div>
@@ -104,7 +189,7 @@ export default function BookRegister(){
                         </div>
 
                         <div className="flex justify-center mt-8">
-                            <FilledButton text={"Cadastrar livro"} type="submit" />
+                            <FilledButton text={"Cadastrar livro"} type="submit"/>
                         </div>
                     </form>
                 </section>
--- src/pages/EditBook/EditBook.jsx
+++ src/pages/EditBook/EditBook.jsx
@@ -2,13 +2,90 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
+import url from "../../services/url";
+import api from "../../services/api";
+
 import MainLayout from "../../layouts/MainLayout";
 import { Input } from "../../components/Input";
 import FilledButton from "../../components/FilledButton";
 
 import { BsArrowLeftShort } from 'react-icons/bs';
+import { getBookById, getGenre } from "../../services/bookService";
 
 export default function EditBook(){
+    const [genres, setGenres] = useState([]);
+    const [book, setBook] = useState([]);
+    const [imagem, setImagem] = useState(null);
+    const {uuid} = useParams();
+    const situacaoMap = ["novo", "seminovo", "antigo"];
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchBook = async () => {
+            const categoriesRes = await getGenre();
+            setGenres(categoriesRes.data);
+
+            const response = await getBookById(uuid);
+            setBook(response);
+        }
+        
+        fetchBook();
+    }, [imagem, uuid]);
+    console.log(book);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const response = await api.put(`/livros/${book.uuid}`, 
+                book,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`,
+                    },
+            });
+
+            if(response){
+                navigate('/meus-livros');
+            }
+        }catch(error){
+            console.log("Erro ao editar livro: ", error);
+        }
+    }
+
+    const handleImage = (e) => {
+        const file = e.target.files[0];
+        setImagem(file);
+
+        handleSubmitImagem(file);
+    }
+
+    const handleSubmitImagem = async (file) => {
+        const fd = new FormData();
+
+        fd.append("imagem", file);
+
+        try{
+            const response = await api.post(`/livros/${book.uuid}/imagem`, 
+                fd, 
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`,
+                    },
+                });
+            setImagem(true);
+            navigate(`/editar-livro/${book.uuid}`);
+        }catch(error){
+            console.log("Erro ao editar livro: ", error);
+        }
+    }
+
+    const handleChange = (e) => {
+        setBook({
+            ...book,
+            [e.target.name]: e.target.value,
+        })
+    }
+
     return(
         <>
             <MainLayout>
@@ -25,16 +102,25 @@ export default function EditBook(){
                 </header>
 
                 <section className="md:ml-20 w-[80%] md:w-[720px] xl:w-[1100px]">
-                    <form className="flex flex-col p-8 mb-8 bg-white rounded-sm shadow-cont" onSubmit={}>
+                    <form className="flex flex-col p-8 mb-8 bg-white rounded-sm shadow-cont" onSubmit={handleSubmit}>
                         <div className="flex flex-col md:flex-row xl:grid grid-cols-3">
                             <div className="flex flex-col justify-between mb-10 md:mb-0 md:mr-8 xl:col-span-1 items-center w-[230px] md:w-[230px] h-[330px] md:h-[330px]">
                                 <img 
-                                    src={} 
+                                    src={url + "/tmp/img/livros/" + book.imagem} 
                                     alt="Capa do livro" 
                                     className="w-full h-full rounded-md object-cover"
+                                    onError={(e) => {
+                                        e.target.src = url + "/public/img/livros/default-book.png";
+                                    }}
                                 />
                                 <div className="flex w-[230px] md:w-[230px] mt-10 rounded-full justify-center items-center py-1 px-10 bg-purple-tr text-purple font-semibold cursor-pointer transition duration-400 ease hover:bg-purple-h">
-                                    <input type="file" id="imagem" className="hidden" name="imagem" onChange={} />
+                                    <input 
+                                        type="file" 
+                                        id="imagem" 
+                                        name="imagem"
+                                        className="hidden"
+                                        onChange={handleImage}
+                                    />
                                     <label htmlFor="imagem" className="">Alterar imagem</label>
                                 </div>
                             </div>
@@ -43,23 +129,34 @@ export default function EditBook(){
                                 <div className="flex flex-col">
                                     <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4">
                                         <Input 
-                                            label={"Título"}  
+                                            label={"Título"} 
+                                            name="titulo" 
+                                            value={book.titulo} 
+                                            onChange={handleChange} 
                                         />
                                         <Input 
                                             label={"Autor"} 
+                                            name="autor" 
+                                            value={book.autor} 
+                                            onChange={handleChange} 
                                         />
                                     </div>
                                     <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4">
                                         <Input 
                                             label={"Nº de páginas"} 
+                                            name="paginas" 
+                                            value={book.paginas} 
+                                            onChange={handleChange} 
                                         />
                                         <div className="flex flex-col">
                                             <label htmlFor="category" className="text-lg">Categoria</label>
                                             <select 
+                                                name="categoria_uuid" 
+                                                id="category" 
                                                 onChange={handleChange} 
                                                 className="w-[325px] h-8 mt-1 mb-3 pl-2 border border-gray rounded-sm focus:outline-none focus:border-purple md:w-[190px] xl:w-[340px]">
                                                     {genres.map((categorie) => {
-                                                        return <option key={categorie.uuid} value={categorie.uuid}>
+                                                        return <option key={categorie.uuid} value={categorie.uuid} >
                                                             {categorie.nome}
                                                         </option>
                                                     })}
@@ -73,30 +170,47 @@ export default function EditBook(){
                                     <div className="flex gap-4 my-2">
                                         <label htmlFor="newBook" className="flex gap-2">
                                             <input 
-                                                type="radio"
-                                                checked={book.estado === "novo"} />
-                                            Novo
+                                                type="radio" 
+                                                name="estado" 
+                                                id="newBook" 
+                                                value="novo" 
+                                                checked={book.estado === "novo"} 
+                                                onChange={(e) => {handleChange(e); setBook(prev => ({ ...prev, estado: "novo" }))}}
+                                            />
+                                                Novo
                                         </label>
                                         
                                         <label htmlFor="likeNew" className="flex gap-2">
                                             <input 
-                                                type="radio"
-                                                checked={book.estado === "seminovo"} />
-                                            Seminovo                                                
+                                                type="radio" 
+                                                name="estado" 
+                                                id="likeNew" 
+                                                value="seminovo"
+                                                checked={book.estado === "seminovo"} 
+                                                onChange={(e) => {handleChange(e); setBook(prev => ({ ...prev, estado: "seminovo" }))}}
+                                            />
+                                                Seminovo                                                
                                         </label>
 
                                         <label htmlFor="oldBook" className="flex gap-2">
                                             <input 
-                                            type="radio"
-                                            checked={book.estado === "antigo"} />
-                                            Antigo
+                                                type="radio" 
+                                                name="estado" 
+                                                id="oldBook" 
+                                                value="antigo" 
+                                                checked={book.estado === "antigo"} 
+                                                onChange={(e) => {handleChange(e); setBook(prev => ({ ...prev, estado: "antigo" }))}}
+                                            />
+                                                Antigo
                                         </label>
                                     </div>
 
                                     <div className="flex flex-col">
                                         <label htmlFor="description" className="text-lg mb-1">Descrição sobre o livro</label>
                                         <textarea 
-                                            name="descricao"
+                                            name="descricao" 
+                                            id="description" 
+                                            value={book.descricao} 
                                             rows="5" 
                                             className="resize-none p-2 border border-gray rounded-sm focus:outline-none focus:border-purple">
                                         </textarea>
